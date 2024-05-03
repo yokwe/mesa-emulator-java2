895ec15992812737e88b9408b1b68fb98c1573a0
hasegawa <hasegawa@localhost>
2024-05-03 13:53:06 +0900

    Move methods relate to StackWalker from ClassUtil to StackUtil.

--- src/main/java/yokwe/util/ClassUtil.java
+++ src/main/java/yokwe/util/ClassUtil.java
@@ -2,7 +2,6 @@ package yokwe.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.StackWalker.StackFrame;
 import java.lang.invoke.CallSite;
 import java.lang.invoke.LambdaMetafactory;
 import java.lang.invoke.MethodHandle;
@@ -174,50 +173,6 @@ public final class ClassUtil {
     	return FindClass.find(packageName);
     }
     
-	public static final int OFFSET_CALLER = 2;
-	public static StackFrame getCallerStackFrame(int offset) {
-		int size = offset + 1;
-		StackFrame[] array = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).walk(s -> s.limit(size).toArray(StackFrame[]::new));
-		if (array == null) {
-			logger.error("array == null");
-			throw new UnexpectedException("array == null");
-		}
-		if (array.length != size) {
-			logger.error("Unexpected array length");
-			logger.error("  size   {}", size);
-			logger.error("  array  {}", array.length);
-			for(int i = 0; i < array.length; i++) {
-				logger.error("  {}  {}", i, array[i].toString());
-			}
-			throw new UnexpectedException("Unexpected array length");
-		}
-		
-		return array[offset];
-	}
-	public static StackFrame getCallerStackFrame() {
-		// offset 0 -- getCallerStackFrame(offset)
-		// offset 1 -- getCallerStackFrame()
-		// offset 2 -- caller
-		return getCallerStackFrame(OFFSET_CALLER);
-	}
-	public static String getCallerMethodName() {
-		// offset 0 -- getCallerStackFrame(offset)
-		// offset 1 -- getCallerStackFrame()
-		// offset 2 -- caller
-		return getCallerStackFrame(OFFSET_CALLER).getMethodName();
-	}
-//	public static Class<?> getCallerClass() {
-//		//Class<?> callerClass = java.lang.invoke.MethodHandles.lookup().lookupClass();
-//		Class<?> callerClass = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
-//		return callerClass;
-//	}
-	public static Class<?> getCallerClass() {
-		// offset 0 -- getCallerStackFrame(2)
-		// offset 1 -- getCallerStackFrame()
-		// offset 2 -- caller
-		return getCallerStackFrame(OFFSET_CALLER).getDeclaringClass();
-	}
-
 	
 	// create instance of clazz using args
 	public static class EnumInfo {
--- src/main/java/yokwe/util/LoggerUtil.java
+++ src/main/java/yokwe/util/LoggerUtil.java
@@ -2,9 +2,8 @@ package yokwe.util;
 
 public final class LoggerUtil {
 	public static org.slf4j.Logger getLogger() {
-//		Class<?> callerClass = java.lang.invoke.MethodHandles.lookup().lookupClass();
 //		Class<?> callerClass = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
-		Class<?> callerClass = ClassUtil.getCallerStackFrame(ClassUtil.OFFSET_CALLER).getDeclaringClass();
+		Class<?> callerClass = StackUtil.getCallerStackFrame(StackUtil.OFFSET_CALLER).getDeclaringClass();
 		
 		return org.slf4j.LoggerFactory.getLogger(callerClass);
 	}
